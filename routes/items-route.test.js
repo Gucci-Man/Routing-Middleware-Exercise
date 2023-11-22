79f5362ab7576cc32c8c45fbc2d06c05704d9775
mrtomno <adel.ngo@yahoo.com>
2023-11-22 17:29:46 -0600

    created tests

---
+++ routes/items-route.test.js
@@ -0,0 +1,67 @@
+process.env.NODE_ENV = "test";
+
+const request = require("supertest");
+
+const app = require("../app");
+let items = require("../fakeDb");
+
+// add items into array to test
+let cheetos = { name: "Cheetos", price: "$4.99" }; 
+let doritos = { name: "Doritos", price: "$2.00" };
+
+beforeEach(function() {
+    items.push(cheetos);
+    items.push(doritos);
+});
+  
+afterEach(function() {
+    // make sure this *mutates*, not redefines, `items`
+    items.length = 0;
+});
+// end afterEach
+
+/** GET /items/[name] - return data about one item */
+
+describe("GET /items/:name", function() {
+    test("Gets a single item", async function() {
+      const resp = await request(app).get(`/items/${cheetos.name}`);
+      expect(resp.statusCode).toBe(200);
+  
+      expect(resp.body).toEqual({name: "Cheetos", price: "$4.99"});
+    });
+  
+    test("Responds with 404 if can't find item", async function() {
+      const resp = await request(app).get(`/items/0`);
+      expect(resp.statusCode).toBe(404);
+    });
+});
+  // end
+
+  /** GET /items - return a list of shopping items */
+describe("GET /items", function() {
+    test("Gets entire shopping list", async function() {
+      const resp = await request(app).get(`/items`);
+      expect(resp.statusCode).toBe(200);
+  
+      expect(resp.body).toEqual({items : [{name: "Cheetos", price: "$4.99"}, { name: "Doritos", price: "$2.00" }]});
+    });
+});
+  // end
+
+/** POST /items - create item from data*/
+
+describe("POST /items", function() {
+    test("Creates a new item", async function() {
+      const resp = await request(app)
+        .post(`/items`)
+        .send({
+          name: "Popcorn",
+          price: "$3.50"
+        });
+      expect(resp.statusCode).toBe(201);
+      expect(resp.body).toEqual({
+        added: {name: "Popcorn" ,
+        price: "$3.50"}});
+    });
+  });
+  // end
